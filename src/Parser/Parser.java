e9fcd2e20da07e05d5452a9b3bec4c915bf7b3c7
Bubimaker <lars.terhofte@gmail.com>
2022-01-20 15:01:38 +0100

    Parser Methoden

--- src/Parser/Parser.java
+++ src/Parser/Parser.java
@@ -1,4 +1,50 @@
 package Parser;
 
+
+import java.util.ArrayList;
+
+//Erarbeitet von Tschogge und Lars
 public class Parser {
+
+
+    private String entryString;
+
+
+
+
+
+
+
+
+    public boolean checkEntity(String entityString){
+
+
+        if (!entityString.equals("")){
+
+        return true;
+
+        }
+
+
+        return false;
+    }
+
+    public String[] parseCommand(String entityString){
+
+        if(checkEntity(entityString)){
+
+            return  entityString.split("\\s+");
+
+        }
+            return null;
+
+    }
+
 }
+
+
+
+
+
+
+
