a99cc5b2c6cb1a90b173b359ea3e3ca011bef669
Giotsche <gioele.petrillo@gmail.com>
2022-01-20 15:14:56 +0100

    Test

---
+++ test/Parser/ParserTest.java
@@ -0,0 +1,22 @@
+package Parser;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ParserTest {
+
+    @org.junit.jupiter.api.Test
+    void checkEntity() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void parseWhole() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void commandOnly() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void parseParameter() {
+    }
+}
