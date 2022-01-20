c150d8e3c0cc644045b95771ed93caf5e7ba8e4d
BuildTools <unconfigured@null.spigotmc.org>
2022-01-20 15:14:01 +0100

    readInput

--- src/Console/Console.java
+++ src/Console/Console.java
@@ -1,4 +1,19 @@
 package Console;
-
+import java.util.Scanner;
 public class Console {
+
+    public String readInput(){
+        Scanner scanner = new Scanner(System.in);
+        String hallo = scanner.nextLine();
+        System.out.println(hallo);
+        return hallo;
+    }
+
+    public void start(){
+        readInput();
+    }
+
+    public void exit(){
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 import Console.CommandInvoker;
+import Console.Console;
 
 public class Main {
     public static void main(String[] args) {
         // ablauf methode von console aufrufen, siehe architekturbild im teams chat
+        Console console = new Console();
+        console.start();
 
     }
 }
