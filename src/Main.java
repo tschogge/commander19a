5d3f6c28245d47ec02eba3bef2ad248cd0a041a4
Giotsche <gioele.petrillo@gmail.com>
2022-01-20 14:36:13 +0100

    main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import Console.CommandInvoker;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Main");
+
+        System.out.println(CommandInvoker.answer);
     }
 }
