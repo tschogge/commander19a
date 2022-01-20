1781ef8be149e77fce44e0d0f2d1ad18792bf48f
Giotsche <gioele.petrillo@gmail.com>
2022-01-20 15:34:08 +0100

    Test

--- src/Parser/Parser.java
+++ src/Parser/Parser.java
@@ -3,41 +3,31 @@ package Parser;
 
 //Erarbeitet von Tschogge und Lars
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Parser {
-
-
-    private String entryString;
-
 
     public boolean checkEntity(String entityString) {
 
         return !entityString.equals("");
     }
 
-    public String[] parseWhole(String entityString) {
-
+    public List<String> parseWhole(String entityString) {
         if (checkEntity(entityString)) {
-
-            return entityString.split("\\s+");
-
+            return new ArrayList<String>(List.of(entityString.split("\\s+")));
         }
         return null;
-
     }
 
-    public String commandOnly(String entityString){
-
-
-        return parseWhole(entityString)[0];
+    public String commandOnly(String entityString) {
+        return parseWhole(entityString).get(0);
     }
 
-    public String[] parseParameter(String entityString){
-
-        String[] transferString = new String[parseWhole(entityString).length - 1];
-
-        for (int i = 1; i < parseWhole(entityString).length; i++) {
-            transferString[i - 1] = parseWhole(entityString)[i];
-        }
+    public List<String> parseParameter(String entityString) {
+        List<String> transferString = parseWhole(entityString);
+        transferString.remove(0);
 
         return transferString;
     }
