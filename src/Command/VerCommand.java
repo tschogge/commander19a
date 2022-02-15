b625d7a369deec67dd31b5918f89404fca7a5650
Bubimaker <lars.terhofte@gmail.com>
2022-02-15 09:38:48 +0100

    Version mitte

--- src/Command/VerCommand.java
+++ src/Command/VerCommand.java
@@ -2,5 +2,46 @@ package Command;
 
 public class VerCommand extends Command{
 
-    //Lars und Tschogge
+    public static int requiredArguments = 0;
+
+    private String versionName;
+    private String versionDisplayText = "System is running on:";
+    private String versionNumber;
+
+
+    //Display from OS-System
+    //Display Console-App-Version
+    //Look if linux easily done
+
+
+
+
+
+    public String versionName(){
+
+        return System.getProperty("os.name");
+
+
+    }
+
+    public String versionNumber(){
+
+        return System.getProperty(versionName);
+    }
+
+    public void displayAll(){
+
+        versionName = versionName();
+        versionNumber = versionNumber();
+
+
+        System.out.println(versionDisplayText + " " + versionName + " " + versionNumber );
+    }
+
+
+
+
+
+
+
 }
---
+++ test/Command/VerCommandTest.java
@@ -0,0 +1,19 @@
+package Command;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class VerCommandTest {
+
+    VerCommand verCommand = new VerCommand();
+
+    @Test
+    void VerNumberDisplay(){
+
+
+
+
+
+    }
+}
